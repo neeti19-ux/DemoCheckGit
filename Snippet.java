2ae86db02fd601ad894ed02cf0bf76da8662d939
Neetika Agarwal <neetika.agarwal@ad.infosys.com>
2021-02-04 19:46:01 +0530

    new file added

---
+++ Snippet.java
@@ -0,0 +1,10 @@
+package io.token.bankintegrationsdkapp;
+
+public class Snippet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Heeloo world>>>");
+	}
+
+}
